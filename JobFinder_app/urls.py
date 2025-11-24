8f75fa1513f5c411def253a4568b0dc41e8ef844
Liteztv <prlutz13@gmail.com>
2025-11-24 18:28:09 -0500

    formwork

--- JobFinder_app/urls.py
+++ JobFinder_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home_view)
+    path('home', views.home_view),
+    path('seeker', views.seeker_view)
 ]
--- JobFinder_app/views.py
+++ JobFinder_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import SeekerForm
+
 
 
 #def base_view(request):
@@ -7,4 +9,12 @@ from django.shortcuts import render
 def home_view(request):
     return render(request,'JobFinder_app/home.html')
 
-# comment
+def seeker_view(request):
+    if request.method == 'POST':
+        form = SeekerForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+    else:
+        form = SeekerForm()
+        return render(request,'JobFinder_app/seekerex.html',context={'form': form})
