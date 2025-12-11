ffd2dcf848f8c60c47dc93d56e945fded1cae90c
Liteztv <prlutz13@gmail.com>
2025-12-11 18:12:12 -0500

    more stuff

--- JobFinder/settings.py
+++ JobFinder/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=02e*y-(+ui@dwr2^!^0cg1ep#us9cj#0_suxs*4!7ey76z8d)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['174.138.84.37']
+ALLOWED_HOSTS = ['174.138.84.37', '127.0.0.1']
 
 
 # Application definition
